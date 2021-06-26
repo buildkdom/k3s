3692b26aada59132c991a71d2dcdd160c06321b2
k3s <k3s@master.example.com>
2021-06-26 13:08:56 +0900

    [Iac] worker1, m1-worker1 scripts and config

---
+++ cluster/m1-worker1/m1-worker1-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+NODE_TOKEN=K10b802a8ef1ec2ff6c571f2adf3ad3dace64184b383135435254171699ad5805e9::server:d48502ab5e235b21381fb10be4f7251e
+MASTER_IP=10.0.2.200
+curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 \
+K3S_TOKEN=$NODE_TOKEN \
+INSTALL_K3S_EXEC="--node-name m1-worker1 --docker" \
+INSTALL_K3S_VERSION="v1.20.0-rc4+k3s1" sh -s –
